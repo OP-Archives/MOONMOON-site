af57cccce74c3dfef7f71a4332fa4d2e25aeabd8
TimIsOverpowered <admin@angelthump.com>
2024-08-15 04:44:33 -0500

    Revert "rm manual"
    This reverts commit 0b043f4c496312139511b644fdc1e4489d91f4c3.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const Vods = lazy(() => import("./vods/Vods"));
 const Navbar = lazy(() => import("./navbar/navbar"));
 const YoutubeVod = lazy(() => import("./vods/YoutubeVod"));
 const Games = lazy(() => import("./games/Games"));
+const CustomVod = lazy(() => import("./vods/CustomVod"));
 const NotFound = lazy(() => import("./utils/NotFound"));
 
 const channel = process.env.REACT_APP_CHANNEL,
@@ -75,6 +76,7 @@ export default function App() {
               <Route exact path="/live/:vodId" element={<YoutubeVod channel={channel} twitchId={twitchId} type="live" VODS_API_BASE={VODS_API_BASE} />} />
               <Route exact path="/youtube/:vodId" element={<YoutubeVod channel={channel} twitchId={twitchId} VODS_API_BASE={VODS_API_BASE} />} />
               <Route exact path="/games/:vodId" element={<Games channel={channel} twitchId={twitchId} VODS_API_BASE={VODS_API_BASE} />} />
+              <Route exact path="/manual/:vodId" element={<CustomVod channel={channel} twitchId={twitchId} type="manual" VODS_API_BASE={VODS_API_BASE} />} />
             </Routes>
           </Suspense>
         </Parent>
