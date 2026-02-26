596769f19e3237f30a642ca04f90e588914500a6
TimIsOverpowered <admin@angelthump.com>
2026-02-26 11:31:00 -0600

    fix build

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,15 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = function override(config, env) {
-  config.plugins.push(
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      generateStatsFile: true,
-      statsFilename: './stats.json',
-    })
-  );
+  if (process.env.ANALYZE === 'true') {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        generateStatsFile: true,
+        statsFilename: './stats.json',
+      })
+    );
+  }
   return config;
 };
